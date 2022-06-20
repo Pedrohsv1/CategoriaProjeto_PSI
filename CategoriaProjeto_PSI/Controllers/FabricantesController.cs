d982643c7d91e4f216718019f689f137fd0d114d
20201011110015 <20201011110015@CNAT391788>
2022-06-20 08:20:24 -0300

    A

--- CategoriaProjeto_PSI/Controllers/FabricantesController.cs
+++ CategoriaProjeto_PSI/Controllers/FabricantesController.cs
@@ -12,14 +12,15 @@ namespace CategoriaProjeto_PSI.Controllers
 {
     public class FabricantesController : Controller
     {
-        private static IList<Fabricante> fabricantes = new List<Fabricante>()
+       /* private static IList<Fabricante> fabricantes = new List<Fabricante>()
         {
             new Fabricante { FabricanteId = 1, Nome = "ITAU" },
             new Fabricante { FabricanteId = 2, Nome = "NUBANK" },
             new Fabricante { FabricanteId = 3, Nome = "PIX" },
             new Fabricante { FabricanteId = 4, Nome = "INTER" },
             new Fabricante { FabricanteId = 5, Nome = "CAIXA" }
-        };
+        };*/
+
         private EFContext context = new EFContext();
         // GET: Fabricantes
         public ActionResult Index()
